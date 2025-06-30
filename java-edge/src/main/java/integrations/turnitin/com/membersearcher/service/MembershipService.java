dceb35033ff3d870c2cbdc76ffaa4fa1f076a3e7
clifford <cliffordmarks@hotmail.com>
2025-06-30 15:19:38 +0100

    updated the code for service, due to having to copy from my original repo to a new one

--- java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
+++ java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
@@ -1,9 +1,14 @@
 package integrations.turnitin.com.membersearcher.service;
 
+import java.util.List;
+import java.util.stream.Stream;
 import java.util.concurrent.CompletableFuture;
 
 import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
+import integrations.turnitin.com.membersearcher.model.Membership;
 import integrations.turnitin.com.membersearcher.model.MembershipList;
+import integrations.turnitin.com.membersearcher.model.User;
+import integrations.turnitin.com.membersearcher.model.UserList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,25 +18,6 @@ public class MembershipService {
 	@Autowired
 	private MembershipBackendClient membershipBackendClient;
 
-	/**
-	 * Method to fetch all memberships with their associated user details included.
-	 * This method calls out to the php-backend service and fetches all memberships,
-	 * it then calls to fetch the user details for each user individually and
-	 * associates them with their corresponding membership.
-	 *
-	 * @return A CompletableFuture containing a fully populated MembershipList object.
-	 */
-	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
-		return membershipBackendClient.fetchMemberships()
-				.thenCompose(members -> {
-					CompletableFuture<?>[] userCalls = members.getMemberships().stream()
-							.map(member -> membershipBackendClient.fetchUser(member.getUserId())
-									.thenApply(member::setUser))
-							.toArray(CompletableFuture<?>[]::new);
-					return CompletableFuture.allOf(userCalls)
-							.thenApply(nil -> members);
-				});
-	}
 
 	/**
 	 * This method simply just finds and returns all the users
@@ -67,4 +53,16 @@ public class MembershipService {
 
 	}
 
+	/**
+	 * Sets the membership linked to the given user, with the user object.
+	 */
+	private void setMemberShip(User user, List<Membership>  memberships) {
+
+		for (Membership member : memberships) {
+			if (member.getUserId().equalsIgnoreCase(user.getId())) {
+				member.setUser(user);
+				break;
+			}
+		}
+	}
 }
